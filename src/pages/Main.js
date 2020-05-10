1201493826681788617bffff574d7c687e8bff14
vitor <vitorn2009@hotmail.com>
2020-05-10 00:15:35 -0300

    Criação de uma feature para sinalizar quando da match.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import {Link} from 'react-router-dom';
+
 import './Main.css';
+
 import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png'
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(false)
 
     useEffect(() => {
         async function loadUsers() {
@@ -22,6 +27,19 @@ export default function Main({ match }) {
 
         loadUsers();
     }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localHost:3333',{
+            query: {
+                user: match.params.id
+            }
+        });
+
+        socket.on('match', dev =>{
+            setMatchDev(dev);
+        })
+
+    },[match.params.id]);
 
     async function handleLike(id){
         await api.post(`/devs/${id}/likes`,null,{
@@ -64,6 +82,17 @@ export default function Main({ match }) {
               </ul>
              ) : (
              <div className="empty"> Acabou </div>   
+            )}{
+            matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt=""/>
+
+                    <img className="avatar" src={matchDev.avatar} alt=""/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type="button" onClick={() => setMatchDev(false)}>Fechar</button>
+                </div>
             )}
         </div>
 
